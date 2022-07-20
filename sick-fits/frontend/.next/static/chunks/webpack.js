cd6b1313562fefafa4a4ddd9210d9f14a3834646
dzhambov <dzhambov@hotmail.com>
2022-07-20 11:13:24 -0400

    add basic styling to navbar.

--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba692517cb5dfb0084be"; }
+/******/ 		__webpack_require__.h = function() { return "ac260be223f0143641b9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
